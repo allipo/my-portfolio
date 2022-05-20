ac3195fa707c84b0e6a127b2ab727c2c425f7a80
allipo <pestotnikall@yahoo.com>
2022-05-19 18:01:45 -0600

    more updates to dog food case study

--- src/pages/case-studies-pages/case-study-2.js
+++ src/pages/case-studies-pages/case-study-2.js
@@ -16,6 +16,19 @@ import persona1 from "./study-2-assets/persona1.jpg";
 import persona2 from "./study-2-assets/persona2.jpg";
 import empathyMap from "./study-2-assets/empathyMap1.jpg";
 import userJourney from "./study-2-assets/userJourneyMap1.jpg";
+import siteMap from "./study-2-assets/siteMap1.jpg";
+import digitalWireframe1 from "./study-2-assets/digital_wireframe_1.jpg";
+import digitalWireframe2 from "./study-2-assets/digital_wireframe_2.jpg";
+import digitalWireframe3 from "./study-2-assets/digital_wireframe_3.jpg";
+import digitalWireframe4 from "./study-2-assets/digital_wireframe_4.jpg";
+import digitalWireframe5 from "./study-2-assets/digital_wireframe_5.jpg";
+import digitalWireframe6 from "./study-2-assets/digital_wireframe_6.jpg";
+import lowFiPrototype from "./study-2-assets/lowFiPrototype.jpg";
+import desktopMockup1 from "./study-2-assets/desktopMockup1.jpg";
+import desktopMockup2 from "./study-2-assets/desktopMockup2.jpg";
+import desktopMockup3 from "./study-2-assets/desktopMockup3.jpg";
+import desktopMockup4 from "./study-2-assets/desktopMockup4.jpg";
+import desktopMockup5 from "./study-2-assets/desktopMockup5.jpg";
 
 const responsibilities = ["User research", "UX design", "Testing"];
 const previewImages = [
@@ -23,7 +36,12 @@ const previewImages = [
   { src: previewImage2, alt: "" },
   { src: previewImage3, alt: "" },
 ];
-const painPoints = ["Pain point 1", "pain point 2", "pain point 3"];
+const painPoints = [
+  "1) Dog food brands don’t always provide enough information about health and environmental concerns for users to feel confident.",
+  "2) Going to the pet supply store takes a lot of time and bags of dog food are heavy to carry home.",
+  "3) Dog food websites don’t always have customization of recommendations based on the individual needs of a dog or its owner.",
+  "4) Not all dog food ordering sites have an option for recurring purchases.",
+];
 const personasArr = [
   { src: persona1, alt: "" },
   { src: persona2, alt: "" },
@@ -31,6 +49,39 @@ const personasArr = [
 const journeyAndEmpathyMapsArr = [
   { src: empathyMap, alt: "" },
   { src: userJourney, alt: "" },
+];
+const siteMapArr = [{ src: siteMap, alt: "" }];
+
+const digitalWireframesArr = [
+  { src: digitalWireframe1, alt: "" },
+  { src: digitalWireframe2, alt: "" },
+  { src: digitalWireframe3, alt: "" },
+  { src: digitalWireframe4, alt: "" },
+  { src: digitalWireframe5, alt: "" },
+  { src: digitalWireframe6, alt: "" },
+];
+
+const lowFiPrototypeArr = [{ src: lowFiPrototype, alt: "" }];
+
+const usabilityStudyParams = [
+  "Type - Moderated usability study",
+  "Participants - 4 dog owners",
+  "Session Length - 20-30 minutes",
+];
+
+const studyFindings = [
+  "1) The meaning of the “order” tab was unclear to users.",
+  "2) Users wanted more information about dog food, such as bag size and portion recommendations.",
+  "3) Recurring order interval options didn't match users' preferred intervals.",
+  "4) Users didn't realize there was content below the fold on some pages.",
+];
+
+const desktopMockupsArr = [
+  { src: desktopMockup1, alt: "" },
+  { src: desktopMockup2, alt: "" },
+  { src: desktopMockup3, alt: "" },
+  { src: desktopMockup4, alt: "" },
+  { src: desktopMockup5, alt: "" },
 ];
 
 function CaseStudy2() {
@@ -46,13 +97,22 @@ function CaseStudy2() {
           lineTopic="Product:"
           lineContent="A responsive website for an organic dog food brand to let users select and order dog food"
         />
-        <Line lineTopic="Problem:" lineContent="asdfadsfadsfsdfs" />
-        <Line lineTopic="Goal:" lineContent="asdfasdfasdf" />
+        <Line
+          lineTopic="Problem:"
+          lineContent="Users need an easy way to find out more about dog food and order it without having to make tedious trips to the pet supply store."
+        />
+        <Line
+          lineTopic="Goal:"
+          lineContent="I tried to make an online experience to satisfy user needs, so that they can get information about dog food options and order dog food all in one place."
+        />
         <List listTopic="Responsibilites:" listContent={responsibilities} />
         <StudyImage srcArr={previewImages} imgLabel="Preview" />
         {/* User Research */}
         <Subtitle subtitle="User Research" />
-        <Line lineTopic="Summary:" lineContent="SfSDFSDFSDfdSf" />
+        <Line
+          lineTopic="Summary:"
+          lineContent="Before ideating solutions, I interviewed dog owners about their experiences buying dog food. I found that many research participants were interested in ordering their dog food online to simplify the task, though found decisions regarding dog food overwhelming. A number of areas of potential improvement were discovered through the interviews."
+        />
         <List listTopic="Pain Points:" listContent={painPoints} />
         <StudyImage srcArr={personasArr} imgLabel="Personas" />
         <Line
@@ -63,6 +123,21 @@ function CaseStudy2() {
           srcArr={journeyAndEmpathyMapsArr}
           imgLabel="Empathy Map, User Journey"
         />
+        {/* Early Designs */}
+        <Subtitle subtitle="Early Designs" />
+        <StudyImage srcArr={siteMapArr} imgLabel="Site Map" />
+        <StudyImage
+          srcArr={digitalWireframesArr}
+          imgLabel="Digital Wireframes"
+        />
+        <StudyImage srcArr={lowFiPrototypeArr} imgLabel="Low-Fi Prototype" />
+        {/* Usability Study */}
+        <Subtitle subtitle="Usability Study" />
+        <List listTopic="Parameters:" listContent={usabilityStudyParams} />
+        <List listTopic="Findings:" listContent={studyFindings} />
+        {/* Refined Designs */}
+        <Subtitle subtitle="Refining the Designs" />
+        <StudyImage srcArr={desktopMockupsArr} imgLabel="Mockups, Desktop" />
       </div>
       <Footer />
     </>
