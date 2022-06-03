12adf95553279ae0b9a9e2990737d112098f0ba7
allipo <pestotnikall@yahoo.com>
2022-06-03 09:19:58 -0600

    changed resume to jpg because wasnt working as pdf

--- src/pages/resume/resume.js
+++ src/pages/resume/resume.js
@@ -3,7 +3,8 @@ import NavBar from "../../components/nav-bar/navbar";
 import PageTitle from "../../components/page-title/PageTitle";
 import Footer from "../../components/footer/footer";
 import GoToTop from "../../components/go-to-top/goToTop";
-import resume from "./resume2022.pdf";
+// import resume from "./resume2022.pdf";
+import resume from "./resume2022.jpg";
 import "./resume.css";
 
 let resumeTitle = "Resumé";
@@ -14,11 +15,12 @@ function Resume() {
       <NavBar />
       <PageTitle title={resumeTitle} />
       <div id="resume-page-body">
-        <iframe
+        <img src={resume} alt="resume jpg" id="resume-pic" />
+        {/* <iframe
           src={resume + "#view=FitH"}
           title="resume pdf"
           id="resumeFrame"
-        />
+        /> */}
       </div>
       <Footer />
       <GoToTop />
