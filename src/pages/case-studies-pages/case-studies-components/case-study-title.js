97aabf6c3fe11137d6f148845754d3ed09ab7b2c
allipo <pestotnikall@yahoo.com>
2022-06-03 08:55:10 -0600

    made case study label a link to case study page

--- src/pages/case-studies-pages/case-studies-components/case-study-title.js
+++ src/pages/case-studies-pages/case-studies-components/case-study-title.js
@@ -1,11 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 import "../case-studies-pages.css";
 
 function StudyTitle(props) {
   return (
     <>
       <div className="study-label-holder">
-        <h2 className="study-label">UX Case Study</h2>
+        <Link to="/casestudies">
+          <h2 className="study-label">UX Case Study</h2>
+        </Link>
       </div>
       <div className="study-name-holder">
         <h1 className="study-name">{props.title}</h1>
